4d72ed37c297e51637bd222e67a93129720ca7d7
makarenko009 <konmakarenko@outlook.com>
2025-06-29 03:08:30 +0300

    average changes - step 1.4 - respond to the /start command and display a welcome message

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+
+import static org.springframework.jdbc.datasource.init.DatabasePopulatorUtils.execute;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -29,8 +33,21 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+            String text = update.message().text();
+            if (text.contains("/start")) {
+                Long id = update.message().chat().id();
+                sendText(id, "Welcome to the bot!");
+            }
         });
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    public void sendText(Long who, String what) {
+        SendMessage request = new SendMessage(who, what);
+        SendResponse response = telegramBot.execute(request);
+        if (!response.isOk()) {
+            logger.error("Failed to send message: {}", response.description());
+        }
+    }
 }
