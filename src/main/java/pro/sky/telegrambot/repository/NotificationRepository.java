a77bcfb0670690406c4676a3996400d2e6706369
makarenko009 <konmakarenko@outlook.com>
2025-06-29 06:32:14 +0300

    average changes - step 2.3 - notification task "Create repository for objects"

--- src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
@@ -2,8 +2,17 @@ package pro.sky.telegrambot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface NotificationRepository /*extends JpaRepository*/ {
-//    @Query(value = "SELECT   notification_text,   LENGTH(name) AS length FROM faculty ORDER BY length DESC LIMIT 1;", nativeQuery = true)
-//    String getLongestFacultyName();
+import java.time.LocalDateTime;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Object, Long> {
+    @Query(value = "INSERT INTO notification_task (message_chat_id, notification_text, date_time) VALUES (:chatId, :text, :time)", nativeQuery = true)
+    void insertNotification(@Param("chatId") Long chatId,
+                            @Param("text") String text,
+                            @Param("time") LocalDateTime time);
 }
+//    @Query(value = "SELECT nt.* FROM public.notification_task AS nt,   LENGTH(name) AS length FROM faculty ORDER BY length DESC LIMIT 1;", nativeQuery = true)
+
