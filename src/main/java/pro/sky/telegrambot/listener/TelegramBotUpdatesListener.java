722e19f15ee1ae87fb86b6ad9d549cdd1ffa048d
makarenko009 <konmakarenko@outlook.com>
2025-06-29 16:46:16 +0300

    average changes - step 2.4 - parsing messages with the formation of reminders and saving them in the database

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -9,19 +9,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.repository.NotificationRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import static org.springframework.jdbc.datasource.init.DatabasePopulatorUtils.execute;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    private final Pattern messagePattern = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4}\\s\\d{2}:\\d{2})(\\s+)(.+)");
 
     @Autowired
     private TelegramBot telegramBot;
+
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+    @Autowired
+    private NotificationRepository notificationRepository;
 
     @PostConstruct
     public void init() {
@@ -33,11 +43,28 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+
             String text = update.message().text();
-            if (text.contains("/start")) {
-                Long id = update.message().chat().id();
-                sendText(id, "Welcome to the bot!");
+            Long id = update.message().chat().id();
+            if ("/start".equals(text)) {
+                sendText(id, "Добро пожаловать в бота! Введите напоминание в формате '01.01.2022 20:00 Сделать домашнюю работу'");
+                return;
             }
+
+            Matcher matcher = messagePattern.matcher(text);
+            if (!matcher.find()) {
+                sendText(id, "Неверный формат сообщения. Введите в виде: '01.01.2022 20:00 Сделать домашнюю работу', отдельным сообщением");
+                return;
+            }
+
+            if (matcher.matches()) {
+                String datetimeStr = matcher.group(1);
+                String reminderText = matcher.group(3);
+                LocalDateTime dateTime = LocalDateTime.parse(datetimeStr, dateTimeFormatter);
+                notificationRepository.insertNotification(id, reminderText, dateTime);
+                sendText(id, "Напоминание сохранено!");
+            }
+
         });
 
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
@@ -46,6 +73,10 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public void sendText(Long who, String what) {
         SendMessage request = new SendMessage(who, what);
         SendResponse response = telegramBot.execute(request);
+        logResponseExc(response);
+    }
+
+    private void logResponseExc(SendResponse response) {
         if (!response.isOk()) {
             logger.error("Failed to send message: {}", response.description());
         }
--- src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationRepository.java
@@ -1,18 +1,22 @@
 package pro.sky.telegrambot.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Object, Long> {
-    @Query(value = "INSERT INTO notification_task (message_chat_id, notification_text, date_time) VALUES (:chatId, :text, :time)", nativeQuery = true)
-    void insertNotification(@Param("chatId") Long chatId,
-                            @Param("text") String text,
-                            @Param("time") LocalDateTime time);
-}
-//    @Query(value = "SELECT nt.* FROM public.notification_task AS nt,   LENGTH(name) AS length FROM faculty ORDER BY length DESC LIMIT 1;", nativeQuery = true)
+public class NotificationRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public NotificationRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertNotification(Long chatId, String text, LocalDateTime time) {
+        String sql = "INSERT INTO notification_task (message_chat_id, notification_text, date_time) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, chatId, text, Timestamp.valueOf(time));
+    }
+}
